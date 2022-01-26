ce07d98fe9b7e58b1fc8ed1bc8a96f196aad40b8
burakarikboga <barikboga42@gmail.com>
2022-01-26 16:55:24 +0300

    day09 if statements warm up

--- src/day09_IfStatements/MinNumber.java
+++ src/day09_IfStatements/MinNumber.java
@@ -5,7 +5,7 @@ public class MinNumber {
         int n1 = 10, n2 = 20;
         if (n1 == n2) System.out.println("Equal");
         if (n1 < n2) System.out.println(n1 + " is the minimum number");
-        if (n2 < n1) System.out.println(n2 + " is the minimum number");
+        else System.out.println(n2 + " is the minimum number");
 
     }
 }
