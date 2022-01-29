6962c602eb7c855ba77f7e3cdb27d2f63a5b0644
Burak Arıkboğa <barikboga42@gmail.com>
2022-01-29 20:18:24 +0300

    OddOrEven updated in Live Session

--- Replit/TwoStatements/OddOrEven.java
+++ Replit/TwoStatements/OddOrEven.java
@@ -4,12 +4,11 @@ import java.util.Scanner;
 
 public class OddOrEven {
     public static void main(String[] args) {
-        //DO NOT TOUCH THESE LINES. TEACHER NEEDS THEM TO TEST YOUR CODE:
-        // Variables are already declared and given
+
         Scanner input = new Scanner(System.in);
         System.out.println("Enter a number:");
         int number = input.nextInt();
-        //CODE HERE:
+
         System.out.print(number % 2 == 0 ? number + " is even" : number + " is odd");
     }
 }
